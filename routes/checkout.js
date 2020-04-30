63df585e572851461d4e82639e158f5ffbbbfa6d
carolina chamorro <cchamorrosaldana@uk-fvfc21jzl414.uk.deloitte.com>
2020-04-30 14:12:09 +0100

    first commit

---
+++ routes/checkout.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var braintree = require('braintree');
+
+router.post('/', function(req, res, next) {
+  var gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox,
+    // Use your own credentials from the sandbox Control Panel here
+    merchantId: '7w7bb5p6hhvvjzqx',
+    publicKey: 'xrnp3fhy4y62xrfm',
+    privateKey: 'c0d0756d9265bb22095902fdad276e30'
+  });
+
+  // Use the payment method nonce here
+  var nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for $10
+  var newTransaction = gateway.transaction.sale({
+    amount: '10.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, function(error, result) {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
+});
+
+module.exports = router;
